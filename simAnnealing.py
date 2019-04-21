0816cd8b9fe34294bc68396849c78f988c6bd583
brunoschubert <brunoschubertm@gmail.com>
2019-04-21 15:46:16 -0300

    Finished Main Program

--- simAnnealing.py
+++ simAnnealing.py
@@ -1,45 +1,63 @@
 import numpy as np
 from random import random, randint, choice
 from readFiles import readFile
-
-# TODO: Lower Colors Use
-# TODO: Optimize Anneal
-
 # ABSOLUTE PATH TO INSTANCES
-# fileToRead = "D:/Unisinos/Inteligência Artificial/instâncias/GCP/test.col.txt"
-# fileToRead = "D:/Unisinos/Inteligência Artificial/instâncias/GCP/games120.col.txt" #9
-# fileToRead = "D:/Unisinos/Inteligência Artificial/instâncias/GCP/myciel6.col.txt" #7
-fileToRead = "D:/Unisinos/Inteligência Artificial/instâncias/GCP/anna.col.txt"  # 11
+test = "D:/Unisinos/Inteligência Artificial/instâncias/GCP/test.col.txt"
+games120 = "D:/Unisinos/Inteligência Artificial/instâncias/GCP/games120.col.txt"  # 9
+myciel6 = "D:/Unisinos/Inteligência Artificial/instâncias/GCP/myciel6.col.txt"  # 7
+anna = "D:/Unisinos/Inteligência Artificial/instâncias/GCP/anna.col.txt"  # 11
 
-# ANNEALING ALGORITHM
+# MAIN PROGRAM
 
 
-def anneal(file):
-    adjMatrix, numVertices = readFile(file)
-    numColors = 15
-    numColors -= 1
-    T = 1.0
-    T_min = 0.0001
+def colorGraph(file):
+    # Define starting parameters
+    file = file
+    numColors = 12
+    temperature = 1.0
+    minTemperature = 0.0001
     alpha = 0.9
     steps = 100
+    cost = 0
+    # While the algorithm can reach a cost of zero
+    # decreases the number of colors
+    while(cost == 0):
+        solution, cost = anneal(
+            file, numColors, temperature, minTemperature, alpha, steps)
+        if(cost == 0):
+            numColors -= 1
+    print("FINAL-> " + "Solution: " + str(solution) + " / " +
+          " Cost: " + str(cost) + " / " + " Colors: " + str(numColors))
+
+
+# ANNEALING ALGORITHM
+def anneal(file, numColors, temperature, minTemperature, alpha, steps):
+    adjMatrix, numVertices = readFile(file)
+    numColors = numColors
+    numColors -= 1
+    temperature = temperature
+    minTemperature = minTemperature
+    alpha = alpha
+    steps = steps
     solution = generateSolution(numVertices, numColors)
     cost, startNeighbour = checkCost(adjMatrix, solution)
-    while T > T_min and cost != 0:
+    while temperature > minTemperature and cost != 0:
         i = 1
         while i <= steps:
             newSolution = genNeighbour(
                 solution, numColors, numVertices, startNeighbour)
             newCost, startNeighbour = checkCost(
                 adjMatrix, solution)
-            ap = acceptance(cost, newCost, T)
+            ap = acceptance(cost, newCost, temperature)
             if ap > random():
                 solution = newSolution
                 cost = newCost
             i += 1
             print("Solution: " + str(solution) + " / " + " Cost: " +
-                  str(cost) + " / " + " Color: " + str(numColors) + " / " + " Temp: " + str(T))
-        T = T*alpha
-    return solution, cost, numColors
+                  str(cost) + " / " + " Color: " + str(numColors + 1) + " / " + " Temp: " + str(temperature))
+        temperature = temperature*alpha
+    numColors += 1
+    return solution, cost
 
 # Generate Acceptance Probability based on the function:
 #  Acceptance = e*((newCost - oldCost) / Temperature)
@@ -53,6 +71,7 @@ def acceptance(oldCost, newCost, temperature):
         return ap
 
 # Calculates cost of the the solution based on the number of collisions
+# and which collision to process
 
 
 def checkCost(adjMatrix, solution):
@@ -102,6 +121,14 @@ def genNeighbour(solution, numColors, numVertices, startNeighbour):
 
 
 # TESTS
-a, b, c = anneal(fileToRead)
-print("FINAL-> " + "Solution: " + str(a) + " / " +
-      " Cost: " + str(b) + " / " + " Colors: " + str(c))
+# numColors = 16
+# temperature = 1.0
+# minTemperature = 0.0001
+# alpha = 0.9
+# steps = 100
+# a, b, c = anneal(myciel6, numColors, temperature,
+#                  minTemperature, alpha, steps)
+# print("FINAL-> " + "Solution: " + str(a) + " / " +
+#       " Cost: " + str(b) + " / " + " Colors: " + str(c))
+
+colorGraph(anna)
