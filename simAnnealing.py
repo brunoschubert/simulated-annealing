3bb575401b88920a1359fed4756c7dd0d5ff0acb
brunoschubert <brunoschubertm@gmail.com>
2019-04-21 02:03:04 -0300

    Finished First Version

--- simAnnealing.py
+++ simAnnealing.py
@@ -1,6 +1,7 @@
 from random import random
 from random import randint
 import numpy as np
+from readFiles import readFile
 
 # TODO: Integrate Graph Coloring problem into algorithm {Define temperatures, steps, etc}
 # TODO: Define final Cost function
@@ -15,67 +16,74 @@ fileToRead = "D:/Unisinos/Inteligência Artificial/instâncias/GCP/test.col.txt"
 # fileToRead = "D:/Unisinos/Inteligência Artificial/instâncias/GCP/games120.col.txt"
 
 
-def anneal(starter):
-    sol = neighbor_solution(starter)
-    old_cost = calculate_cost(sol)
+def anneal(file):
+    adjMatrix, numVertices = readFile(file)
+    numColors = 15
     T = 1.0
     T_min = 0.00001
     alpha = 0.9
     steps = 1000
+    solution = generateSolution(numVertices, numColors)
+    cost, startNeighbour = checkCost(adjMatrix, solution, numVertices)
+    if(startNeighbour == -1):
+        startNeighbour = 0
     while T > T_min:
         i = 1
         while i <= steps:
-            new_sol = neighbor_solution(sol)
-            new_cost = calculate_cost(new_sol)
-            ap = acceptance(old_cost, new_cost, T)
+            newSolution = genNeighbour(solution, startNeighbour, numColors)
+            newCost, startNeighbour = checkCost(
+                adjMatrix, solution, numVertices)
+            ap = acceptance(cost, newCost, T)
             if ap > random():
-                sol = new_sol
-                old_cost = new_cost
+                solution = newSolution
+                cost = newCost
             i += 1
-            print("Solution: " + str(sol) + " / " + " Cost: " + str(old_cost))
+            print("Solution: " + str(solution) + " / " + " Cost: " + str(cost))
         T = T*alpha
-    return sol, old_cost
+    return solution, cost
 
 # Generate Acceptance Probability based on the function:
-#  Acceptance = e*((cost_new - cost_old) / Temperature)
+#  Acceptance = e*((newCost - oldCost) / Temperature)
 
 
-def acceptance(cost_old, cost_new, Temp):
-    if cost_new < cost_old:
+def acceptance(oldCost, newCost, temperature):
+    if newCost < oldCost:
         return 1
     else:
-        p = np.exp(- (cost_new - cost_old) / Temp)
-        # print(str(p))
+        p = np.exp((newCost - oldCost) / temperature)
         return p
 
 
 def checkCost(adjMatrix, solution, numVertices):
+    cost = 0
+    startNeighbour = -1
     for vert in range(numVertices):
-        cost = 0
         for i in range(len(adjMatrix)):
             for j in range(len(adjMatrix[i])):
                 if(adjMatrix[i][j] == 1 and solution[vert] == solution[i]):
                     cost += 1
-    return cost
+                    startNeighbour = vert
+    return cost, startNeighbour
 
 # Se na matriz marcar 1, olha na posição da solução pra ver se tem conflito
 
 
-def generateSolution(solution, numVertices, numColors):
+def generateSolution(numVertices, numColors):
+    solution = np.arange(numVertices)
     for i in range(numVertices):
         solution[i] = randint(0, numColors)
     return solution
 
 
 def genNeighbour(solution, startNeighbour, numColors):
-    oldSol = solution[startNeighbour]
-    newSol = randint(0, numColors)
-    while (newSol == oldSol):
-        newSol = randint(0, numColors)
-    solution[startNeighbour] = newSol
+    oldColor = solution[startNeighbour]
+    newColor = randint(0, numColors)
+    while (newColor == oldColor):
+        newColor = randint(0, numColors)
+    solution[startNeighbour] = newColor
     return solution
 
 
 # TESTS
-a, b = anneal(100)
+a, b = anneal(fileToRead)
 print("FINAL-> " + "Solution: " + str(a) + " / " + " Cost: " + str(b))
